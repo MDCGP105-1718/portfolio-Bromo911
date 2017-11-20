5bae49b082d83fb79c59b1ac5a0831ff36d32cd6
Bromo911 <32357053+Bromo911@users.noreply.github.com>
2017-11-20 16:32:27 +0000

    Completed bottle exercise

--- Python/ex5v3.py
+++ Python/ex5v3.py
@@ -1,9 +1,16 @@
-bottle_number = 99
-if ({bottle_number} != 0
-print(f"1{bottle_number} bottles of beer on the wall, {bottle_number} bottles of beer.")
-(bottle_number) - 1
-print(f"Take one down, pass it around, {bottle_number} bottles of beer on the wall…")
-)
-else(
-print(f"No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall…")
-)
+bottle_number = 0
+
+
+for n in range(99,0,-1):
+    if bottle_number == 2:
+        print(f"{bottle_number} bottles of beer on the wall, {bottle_number} bottles of beer.")
+        bottle_number = n - 1
+        print(f"Take one down, pass it around, {bottle_number} bottle of beer on the wall…")
+    if bottle_number == 1:
+        print ("1 bottle of beer on the wall, 1 bottle of beer! So take it down, pass it around, no more bottles of beer on the wall!")
+        bottle_number = n - 1
+        break
+    bottle_number = n
+    print(f"{bottle_number} bottles of beer on the wall, {bottle_number} bottles of beer.")
+    bottle_number = n - 1
+    print(f"Take one down, pass it around, {bottle_number} bottles of beer on the wall…")
